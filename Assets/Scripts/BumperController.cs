d64909b249f6fd53c4d020628aac001a1487a873
Hisbul Wathoni <hisbulwathoni663@gmail.com>
2023-07-16 11:48:08 +0800

    Switch, Ramp, Launcher

--- Assets/Scripts/BumperController.cs
+++ Assets/Scripts/BumperController.cs
@@ -8,6 +8,8 @@ public class BumperController : MonoBehaviour
     public Color color;
     private Renderer render;
 
+    private Animator animator;
+
 
     public float multiplier;
     public Collider bola;
@@ -15,12 +17,16 @@ public class BumperController : MonoBehaviour
     {
         render = GetComponent<Renderer>();
         render.material.color = color;
+
+        animator = GetComponent<Animator>();
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider == bola)
         {
             Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
+
+            animator.SetTrigger("Hit");
 
             bolaRig.velocity *= multiplier;
             Debug.Log("Kena Bola");
