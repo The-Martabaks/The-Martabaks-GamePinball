9cce18518278f9bf67b5d0b03df59d2a43e415e0
hungercodes <medeline17@gmail.com>
2023-07-24 22:19:24 +0700

    Add coin spawn point

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public Transform[] coinSpawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinSpawnPoint = new Transform[5];
+
     }
 
     // Update is called once per frame
