3adb6e3b2b78f6ba1d37e6260d1bfb4acff1369b
hungercodes <medeline17@gmail.com>
2023-07-24 17:24:09 +0700

    Revert "Fix script"
    This reverts commit b3e5d56a492ce6f366a946eea36a9a04c1244259.

--- Assets/Scripts/SwithController.cs
+++ Assets/Scripts/SwithController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchController : MonoBehaviour
+public class SwithController : MonoBehaviour
 {
     private enum SwitchState
     {
@@ -14,7 +14,7 @@ public class SwitchController : MonoBehaviour
     // Menyimpan collider bola
     public Collider bola;
 
-    // Menyimpan variabel material nyala dan mati untuk merubah warna
+    // Menimpan variabel material nyala dan mati untuk merubah warna
     public Material offMaterial;
     public Material onMaterial;
 
