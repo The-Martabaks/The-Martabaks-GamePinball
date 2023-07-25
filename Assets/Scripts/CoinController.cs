581c4508b23461c42e5f06712965381646a157ef
hungercodes <medeline17@gmail.com>
2023-07-25 12:43:16 +0700

    Coin spawn & destroy
    Berhasil spawn & destroy, problem: spawn di titik sama, coroutine belom bener.

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,17 +4,95 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    public Transform[] coinSpawnPoint;
-    // Start is called before the first frame update
-    void Start()
+    private enum SwitchState
     {
-        coinSpawnPoint = new Transform[5];
-
+        Off,
+        On,
+        Add
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private GameObject coinPrefab;
+
+    private GameObject coinClone;
+    private int spawnIndex;
+
+    [SerializeField]
+    private Transform[] coinSpawnPoint;
+
+    // Menyimpan collider bola
+    public Collider bola;
+
+    // Menggantikan isOn
+    private SwitchState state;
+    private int maxCoin = 3;
+    private int coinCounter = 0;
+
+    private void Start()
     {
-        
+        Set(true);
+        StartCoroutine(CoinTimerOff(10));
+    }
+
+    void spawnCoin()
+    {
+        spawnIndex = Random.Range(0, coinSpawnPoint.Length);
+        coinClone = Instantiate(coinPrefab, coinSpawnPoint[spawnIndex].position, coinPrefab.transform.rotation);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Memastikan yang menabrak adalah bola
+        if (other == bola)
+        {
+            Toggle();
+        }
+    }
+
+    // Fungsi untuk toggle
+    private void Toggle()
+    {
+        if (state == SwitchState.On || state == SwitchState.Add)
+        {
+            Set(false);
+        }
+    }
+
+    private void Set(bool active)
+    {
+        if (active == true)
+        {
+            state = SwitchState.On;
+
+            StartCoroutine(AddCoin(1));
+        }
+        else
+        {
+            state = SwitchState.Off;
+            Destroy(coinClone);
+        }
+    }
+
+    private IEnumerator AddCoin(int times)
+    {
+        state = SwitchState.Add;
+        if (coinCounter < maxCoin)
+        {
+            yield return new WaitForSeconds(3);
+            spawnCoin();
+            Debug.Log("spawn" + coinCounter);
+            coinCounter += 1;
+        }
+    }
+
+
+    // Enum untuk mengatur state
+    private IEnumerator CoinTimerOff(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(coinClone);
+        Debug.Log("coin mati");
+        StartCoroutine(AddCoin(1));
     }
 }
