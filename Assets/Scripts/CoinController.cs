ff9f3f46d3b6ae402a7bef6e9c1204f58f1e794a
hungercodes <medeline17@gmail.com>
2023-07-26 08:27:41 +0700

    Spawn coin done
    Spawn coin solved
    problem: Can't set active false when collide

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,95 +4,76 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    private enum SwitchState
-    {
-        Off,
-        On,
-        Add
-    }
-
     [SerializeField]
     private GameObject coinPrefab;
 
-    private GameObject coinClone;
+    private GameObject coinTemp;
     private int spawnIndex;
 
     [SerializeField]
-    private Transform[] coinSpawnPoint;
+    private GameObject[] coinSpawnPoint;
 
     // Menyimpan collider bola
-    public Collider bola;
+    public List<Collider> coin = new List<Collider>();
 
-    // Menggantikan isOn
-    private SwitchState state;
     private int maxCoin = 3;
     private int coinCounter = 0;
 
     private void Start()
     {
-        Set(true);
-        StartCoroutine(CoinTimerOff(10));
+        StartCoroutine(AddCoin(1));
+    }
+
+    private void Update()
+    {
+        OnCollisionEnter(coin);
     }
 
     void spawnCoin()
     {
         spawnIndex = Random.Range(0, coinSpawnPoint.Length);
-        coinClone = Instantiate(coinPrefab, coinSpawnPoint[spawnIndex].position, coinPrefab.transform.rotation);
-    }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Memastikan yang menabrak adalah bola
-        if (other == bola)
+        if (coinSpawnPoint[spawnIndex].activeSelf == false)
         {
-            Toggle();
-        }
-    }
-
-    // Fungsi untuk toggle
-    private void Toggle()
-    {
-        if (state == SwitchState.On || state == SwitchState.Add)
-        {
-            Set(false);
-        }
-    }
-
-    private void Set(bool active)
-    {
-        if (active == true)
-        {
-            state = SwitchState.On;
-
-            StartCoroutine(AddCoin(1));
+            coinSpawnPoint[spawnIndex].SetActive(true);
+            coinCounter += 1;
         }
         else
         {
-            state = SwitchState.Off;
-            Destroy(coinClone);
+            while (coinSpawnPoint[spawnIndex].activeSelf == true)
+            {
+                spawnIndex = Random.Range(0, coinSpawnPoint.Length);
+            }
+            coinSpawnPoint[spawnIndex].SetActive(true);
+            coinCounter += 1;
+        }
+    }
+
+    private void OnCollisionEnter(Collider other)
+    {
+        // Memastikan yang menabrak adalah bola
+        if (other.gameObject.tag == "coin")
+        {
+            other.gameObject.SetActive(false);
         }
     }
 
     private IEnumerator AddCoin(int times)
     {
-        state = SwitchState.Add;
-        if (coinCounter < maxCoin)
+        for(int i=0; i<maxCoin; i++)
         {
             yield return new WaitForSeconds(3);
             spawnCoin();
             Debug.Log("spawn" + coinCounter);
-            coinCounter += 1;
         }
+        StartCoroutine(CoinTimerOff(10));
     }
 
-
-    // Enum untuk mengatur state
+    // Enum untuk mendestroy koin
     private IEnumerator CoinTimerOff(float time)
     {
         yield return new WaitForSeconds(time);
-        Destroy(coinClone);
+        //this.coinSpawnPoint[].SetActive(false);
+        coinCounter -= 1;
         Debug.Log("coin mati");
-        StartCoroutine(AddCoin(1));
     }
 }
